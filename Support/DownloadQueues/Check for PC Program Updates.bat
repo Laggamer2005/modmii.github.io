7a7803aec185ff4cc39f36e32303b1b98d8780bc
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-11-17 09:18:53 +0000

    Update Check for PC Program Updates.bat

--- Support/DownloadQueues/Check for PC Program Updates.bat
+++ Support/DownloadQueues/Check for PC Program Updates.bat
@@ -1,16 +1,23 @@
-::ModMii v6.6.4 - Download Queue - Tue 02/21/2023 -  8:36:53.01
+:: ModMii v6.6.4 - Download Queue - Tue 02/21/2023 -  8:36:53.01
+
+:: Initialize variables
 set AdvNumber=0
-if /i "%GetAdvNumberOnly%" EQU "Y" goto:endofqueue
-::Queue Settings
-Set ROOTSAVE=off
-Set Option1=off
-::Queue
-SET Dolphin=*
-SET FILEZILLA=*
-SET hxd=*
-SET Diskitude=*
-SET RC24=*
-SET HashMF=*
-SET GCBM=*
-SET OSC=*
+
+:: Check if GetAdvNumberOnly is set to "Y" (case-insensitive)
+if /i "%GetAdvNumberOnly%" EQU "Y" goto endofqueue
+
+:: Queue Settings
+set ROOTSAVE=off
+set Option1=off
+
+:: Queue
+set Dolphin=*
+set FILEZILLA=*
+set hxd=*
+set Diskitude=*
+set RC24=*
+set HashMF=*
+set GCBM=*
+set OSC=*
+
 :endofqueue
